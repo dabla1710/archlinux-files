f099bfca03988a725f7018aa1097619d0c9b6cda
dabla1710 <daniel.blaich@outlook.com>
2022-12-08 00:05:59 +0100

    setup + hypr changes

--- arch_setup_script.sh
+++ arch_setup_script.sh
@@ -1,9 +1,7 @@
 # !/bin/bash
 
-# Temporary Screen Resolution
-xrandr -s 1920x1080
-
 ##### MUST-HAVES
+
 # Install yay
 sudo pacman -S yay
 # Install zsh
@@ -13,11 +11,14 @@ sudo pacman -S zsh --noconfirm --needed
 chsh -s /bin/zsh
 # Install neovim
 sudo pacman -S nvim --no-confirm --needed
-# make dir for nvim
-mkdir ~/.config/nvim
 # Install vim-plug
 yay -S vim-plug
 # ToDo! Autoload nvim config from git ?
+# config files from github, no WM or DE yet
+git clone https://github.com/dabla1710/archlinux-files ~
+cd ~/archlinux-files
+cp -r alacritty nvim waybar neofetch ~/.config/
+cp endless_linux_guide.txt ~
 
 # Installing and updating mlocate
 sudo pacman -S mlocate
@@ -27,12 +28,15 @@ sudo updatedb
 # cp init.vim ~/.config/nvim/
 
 # Install oh-my-zsh (zsh autosetup) (curl and git necessary)
-# # favourite zsh theme "steeef"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel 10k theme (configure with 'p10k configure')
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# alacritty-theme
+git clone https://github.com/eendroroy/alacritty-theme.git ~/.alacritty-colorscheme
+
 
 ##### i3 related
 # do if i3 statement later
@@ -40,23 +44,28 @@ echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 # yay -S autotiling
 
 
-
+##### TODO if cases for WM / DE
 #### Sway related
 # wayland support and helper xlsclients
 sudo pacman -S xorg-xwayland xorg-xlsclients qt5-wayland glfw-wayland
 # exec_always autotling in config
 #yay -S autotling
+
+
 ##### CLI applications
 # cmatrix
 #sudo pacman -S cmatrix
 # ddgr (command line search)
 yay -S ddgr
+
+
 ##### GUI applications
 # discord
 sudo pacman -S discord
+sudo pacman -S thunderbird
 # onlyoffice
 yay -S onlyoffice-bin
-# Mailspring, gnome-keyring & libsecret
-#sudo pacman -S libsecret --no-confirm --needed
-#sudo pacman -S gnome-keyring --no-confirm --needed
-#yay -S mailspring
+yay -S teams
+yay -S skypeforlinux-stable-bin
+yay -S brave-bin
+
