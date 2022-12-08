183d10869809b916ac778b520afbfd251f58bfdc
dabla1710 <daniel.blaich@outlook.com>
2022-12-08 14:24:56 +0100

    waybar hyprland and sway seperated

--- collect_local_files.sh
+++ collect_local_files.sh
@@ -1,6 +1,12 @@
 # endless linux guide
 cp ~/endless_linux_guide.txt ~/archlinux-files/
 
+# waybar - if case for wm
+# on hyprland:
+#cp -r ~/.config/waybar ~/archlinux-files/waybar-hyprland/
+# on sway:
+cp -r ~/.config/waybar ~/archlinux-files/waybar-sway/
+
 # neovim
 cp -r ~/.config/nvim ~/archlinux-files/
 # alacritty
@@ -11,5 +17,3 @@ cp -r ~/.config/hypr ~/archlinux-files/
 cp -r ~/.config/neofetch ~/archlinux-files/
 # sway
 cp -r ~/.config/sway ~/archlinux-files/
-# waybar
-cp -r ~/.config/waybar ~/archlinux-files/
